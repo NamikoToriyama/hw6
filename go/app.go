6c747cde56ba14f86e6851ce2bb5fc80adf80b00
Namiko Toriyama <>
2018-07-02 15:31:06 +0900

    テキストの追加

--- go/app.go
+++ go/app.go
@@ -11,5 +11,16 @@ func init() {
 
 func handlePata(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "Hello STEP!\n")
+	fmt.Fprintf(w, "<div style=\"text-align:center;\">")
+	fmt.Fprintf(w, "<div>Hello STEP!</div>")
+	fmt.Fprintf(w, "<div>word1\n")
+	fmt.Fprintf(w, "<input type=\"text\"></input></div>\n")
+	fmt.Fprintf(w, "<div>word2\n")
+	fmt.Fprintf(w, "<input type=\"text\"></input></div>\n")
+	fmt.Fprintf(w, "<input type=\"button\" value=\"submit\"></input></div>\n")
+	fmt.Fprintf(w, "</div>")
+}
+func test(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "foo is '%s'!", r.FormValue("foo"))
 }
